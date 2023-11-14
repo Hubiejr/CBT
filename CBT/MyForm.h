16f4d8c19c1d94362455fb88f698df74d0ff04b3
Hubiejr <59489351+Hubiejr@users.noreply.github.com>
2023-11-13 23:16:33 -0500

    changed fonts

--- CBT/MyForm.h
+++ CBT/MyForm.h
@@ -75,14 +75,15 @@ namespace CBT {
 			// 
 			// button1
 			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button1->BackColor = System::Drawing::SystemColors::Window;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Ebrima", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->button1->Location = System::Drawing::Point(83, 72);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(136, 33);
 			this->button1->TabIndex = 0;
 			this->button1->Text = L"ADD ITEM:";
-			this->button1->UseVisualStyleBackColor = true;
+			this->button1->UseVisualStyleBackColor = false;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// label1
@@ -91,6 +92,7 @@ namespace CBT {
 			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::MenuText;
 			this->label1->Location = System::Drawing::Point(70, 9);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(149, 29);
@@ -130,9 +132,11 @@ namespace CBT {
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(31, 140);
+			this->label2->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(30, 135);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(50, 16);
+			this->label2->Size = System::Drawing::Size(59, 20);
 			this->label2->TabIndex = 6;
 			this->label2->Text = L"Name: ";
 			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
@@ -140,29 +144,36 @@ namespace CBT {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(18, 185);
+			this->label3->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(12, 182);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(67, 16);
+			this->label3->Size = System::Drawing::Size(78, 20);
 			this->label3->TabIndex = 7;
 			this->label3->Text = L"Due Date:";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
 			this->label4->Location = System::Drawing::Point(31, 234);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(41, 16);
+			this->label4->Size = System::Drawing::Size(48, 20);
 			this->label4->TabIndex = 8;
 			this->label4->Text = L"Time:";
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(4, 286);
+			this->label5->Font = (gcnew System::Drawing::Font(L"Ebrima", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(3, 282);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(81, 16);
+			this->label5->Size = System::Drawing::Size(87, 17);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Description: ";
+			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
 			// 
 			// checkBox1
 			// 
@@ -232,6 +243,8 @@ private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e
 	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 	}
 private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
