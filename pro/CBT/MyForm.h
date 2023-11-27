1e5f838008f0dcbf969d8b4735e190c1c16dc840
AlexIrvin1 <143729917+AlexIrvin1@users.noreply.github.com>
2023-11-26 22:19:22 -0500

    Add files via upload

---
+++ pro/CBT/MyForm.h
@@ -0,0 +1,285 @@
+#pragma once
+#include "TodoList.h"
+#include <list> // Add to include a list it will be easy they say
+namespace CBT {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			// button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click); // added so we can save data later
+			assignmentsList = gcnew System::Collections::Generic::List<ToDo^>();
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Collections::Generic::List<ToDo^>^ assignmentsList; // List to store ToDo forms
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::CheckBox^ checkBox1;
+	private: System::Windows::Forms::CheckBox^ checkBox2;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::Window;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Ebrima", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(83, 72);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(136, 33);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"ADD ITEM:";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft YaHei UI", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::SystemColors::MenuText;
+			this->label1->Location = System::Drawing::Point(70, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(149, 29);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Edit Schedule";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(91, 135);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(110, 22);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(91, 182);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(110, 22);
+			this->textBox2->TabIndex = 3;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox2_TextChanged);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(91, 231);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(110, 22);
+			this->textBox3->TabIndex = 4;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(91, 280);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(110, 22);
+			this->textBox4->TabIndex = 5;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(30, 135);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(59, 20);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"Name: ";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm::label2_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(12, 182);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(78, 20);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Due Date:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Ebrima", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label4->Location = System::Drawing::Point(31, 234);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(48, 20);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Time:";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Ebrima", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(3, 282);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(87, 17);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Description: ";
+			this->label5->Click += gcnew System::EventHandler(this, &MyForm::label5_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(83, 339);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(102, 20);
+			this->checkBox1->TabIndex = 10;
+			this->checkBox1->Text = L"Notifications";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(83, 376);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(126, 20);
+			this->checkBox2->TabIndex = 11;
+			this->checkBox2->Text = L"Nagging Notifier";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::LemonChiffon;
+			this->ClientSize = System::Drawing::Size(277, 425);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->Name = L"MyForm";
+			this->Text = L"EditSchedule";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	ToDo^ form = gcnew ToDo(); // will open new form when hit of the button 
+	form->SetAssignment1(textBox1->Text);
+	form->SetDate1(textBox2->Text);
+	form->SetTime1(textBox3->Text);
+	form->SetDescription1(textBox4->Text);
+	form->Show();
+
+	// adds the ToDo form to the list
+	assignmentsList->Add(form);
+
+	// Gets data entered by User
+	String^ name = textBox1->Text;
+	String^ dueDate = textBox2->Text;
+	String^ time = textBox3->Text;
+	String^ description = textBox4->Text;
+	PrintMessage("Button 1 Pressed!");
+
+	// You can now save this data to a file or a database, or perform any other desired action.
+	// For this example, we'll just display the data in a message box.
+	String^ message = "Name: " + name + "\nDue Date: " + dueDate + "\nTime: " + time + "\nDescription: " + description;
+	MessageBox::Show(message, "Data Saved");
+	
+
+	//// Create a new instance of ToDo for each assignment  DOESNT WORK PROPERLY
+	//ToDo^ form1 = gcnew ToDo();
+	//form1->SetAssignment1(name);
+	//form1->Show();
+	//assignmentsList->Add(form1);
+
+	//ToDo^ form2 = gcnew ToDo();
+	//form2->SetAssignment2(name);
+	//form2->Show();
+	//assignmentsList->Add(form2);
+
+	// clear text boxes after you hit the button
+	textBox1->Clear();
+	textBox2->Clear();
+	textBox3->Clear();
+	textBox4->Clear();
+	
+}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label5_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+	   void PrintMessage(String^ message) {
+		   System::Console::WriteLine(message);
+	   }
+};
+}
